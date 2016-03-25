f54ed3bc8f28175fded4f8d8740801f26952b25b
eemshi <oreneemshi@gmail.com>
2016-03-25 03:39:35 -0600

    fill out sidebar & add a search bar

--- src/cljs/owlet/app.cljs
+++ src/cljs/owlet/app.cljs
@@ -7,24 +7,40 @@
 (defn custom-header []
   (let [img-src (reagent/atom "http://eskipaper.com/images/space-1.jpg")]
     (fn []
-    [:div.custom-header
-      [:img {:src @img-src}]
+    [:div.custom-header.no-gutter
       [:button.btn-primary
-       {:onClick
-        (fn []
-          (let [url (js/prompt "i need a url")]
-              (when url
-                    (reset! img-src url))
-              ))} "change me!"]])))
+      {:onClick
+       (fn []
+         (let [url (js/prompt "i need a url")]
+             (when url
+                   (reset! img-src url))
+             ))} "change me!"]
+      [:img {:src @img-src}]
+      ])))
 
 (defn main []
-  [:div.container-fluid
-    [:div.left.col-lg-3
-      [:h2 "owlet"]
-      [:p "sidebar"]]
-    [:div.right.col-lg-9
+  [:div.no-gutter
+    [:div.left.col-lg-2.text-center
+      [:img {:src "img/owlet-logo.png"}]
+      [:div.options
+        [:h1 "owlet"]
+        [:img {:src "img/icon1.png"}][:br]
+        [:img {:src "img/icon2.png"}][:br]
+        [:img {:src "img/icon3.png"}]
+      ]
+    ]
+    [:div.right.col-lg-10
       [custom-header]
-      [:div.content]]])
+      [:div.search
+        [:input {
+          :type "search"
+          :name "sitesearch"}]
+        [:input {
+          :type "submit"
+          :value "Search"}]
+      ]
+    ]
+      [:div.content]])
 
 
 (defn init []
