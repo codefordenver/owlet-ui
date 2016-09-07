94be1e3d3af286d769cd4c016840012afd0929a7
ZadenRB <zaden.ruggieroboune@gmail.com>
2016-09-07 15:12:26 -0600

    Fixed responsive navigation menu, adding a low profile version for smaller windows

--- src/cljs/owlet_ui/components/sidebar.cljs
+++ src/cljs/owlet_ui/components/sidebar.cljs
@@ -4,25 +4,29 @@
     [reagent.core :refer [atom]]))
 
 (defn sidebar-component []
-        [:div
-          [:span
-            [:button.btn-primary.btn-md {:type        "button"}
-                                        :data-toggle "offcanvas"
-                                        :on-click    #(-> (js/$ ".row-offcanvas")
-                                                          (.toggleClass "active")) "Menu"]]]
-        [:div#sidebar.hidden-xs-down
-          [:a {:href "#/"}
-            [:img.owlet {:src "img/owlet-logo.png"}]]
-          ;; TODO: fix responsive menu
-          [:div.menu
-            [:h1 "owlet"]
-            [:ul.nav.nav-pills.nav-stacked
-              [:li.nav-item
-                [:a.nav-link {:href "#/library"}
-                  [:img {:src "img/icon1.png"}]]]
-              [:li.nav-item
-                [:a.nav-link {:href "#/"}
-                  [:img {:src "img/icon2.png"}]]]
-             [:li.nav-item
-                [:a.nav-link {:href "#/settings"}
-                  [:img {:src "img/icon3.png"}]]]]]])
+  [:div
+    [:div#lpsidebar.hidden-sm-up
+      [:div.menu
+        [:a {:href "#/library"}
+          [:button.btn-primary {:type "button"} "Library"]]
+        [:a {:href "#/"}
+          [:button.btn-primary {:type "button"} "Home"]]
+        [:a {:href "#/settings"}
+          [:button.btn-primary {:type "button"} "Settings"]]]]
+
+    [:div#sidebar.hidden-xs-down
+      [:a {:href "#/"}
+        [:img.owlet {:src "img/owlet-logo.png"}]]
+      [:div.menu
+        [:span
+          [:h1 "owlet"]
+          [:ul.nav.nav-pills.nav-stacked
+            [:li.nav-item
+              [:a.nav-link {:href "#/library"}
+                [:img {:src "img/icon1.png"}]]]
+            [:li.nav-item
+              [:a.nav-link {:href "#/"}
+                [:img {:src "img/icon2.png"}]]]
+            [:li.nav-item
+              [:a.nav-link {:href "#/settings"}
+                [:img {:src "img/icon3.png"}]]]]]]]])
