b810d817a3d344078274ec272c978af9360de222
dviramontes <dviramontes@gmail.com>
2016-09-09 00:45:42 -0600

    remove " Activity" in model activity prop

--- src/cljs/owlet_ui/components/track.cljs
+++ src/cljs/owlet_ui/components/track.cljs
@@ -2,8 +2,9 @@
 
 (defn track [data]
   (fn []
-    [:div.trackwrapper.col-xs-12.col-sm-6.col-lg-4
-     [:div.trackwrap
-      [:a.track {:href "#"}
-       [:h2 (:name data)]
-       [:p (:description data)]]]]))
+    (let [name (.replace (:name data) " Activity" "")]
+      [:div.trackwrapper.col-xs-12.col-sm-6.col-lg-4
+       [:div.trackwrap
+        [:a.track {:href "#"}
+         [:h2 name]
+         [:p (:description data)]]]])))
