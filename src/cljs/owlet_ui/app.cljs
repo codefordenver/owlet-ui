51eb34f49ddcfc7df3cee4bde059c20e4a537b3d
dviramontes <dviramontes@gmail.com>
2016-10-04 00:46:06 -0600

    fix: login component, should remain logged in, delete token when expired

--- src/cljs/owlet_ui/app.cljs
+++ src/cljs/owlet_ui/app.cljs
@@ -1,13 +1,14 @@
 (ns owlet-ui.app
-    (:require [re-frame.core :as re-frame]
-              [owlet-ui.config :as config]
-              [owlet-ui.components.sidebar :refer [sidebar-component]]
-              [owlet-ui.components.header :refer [header-component]]
-              [owlet-ui.views.welcome :refer [welcome-view]]
-              [owlet-ui.views.activity :refer [activity-view]]
-              [owlet-ui.views.tracks :refer [tracks-view]]
-              [owlet-ui.views.settings :refer [settings-view]]
-              [owlet-ui.views.track-activities :refer [track-activities-view]]))
+  (:require [re-frame.core :as re-frame]
+            [reagent.core :as reagent]
+            [owlet-ui.config :as config]
+            [owlet-ui.components.sidebar :refer [sidebar-component]]
+            [owlet-ui.components.header :refer [header-component]]
+            [owlet-ui.views.welcome :refer [welcome-view]]
+            [owlet-ui.views.activity :refer [activity-view]]
+            [owlet-ui.views.tracks :refer [tracks-view]]
+            [owlet-ui.views.settings :refer [settings-view]]
+            [owlet-ui.views.track-activities :refer [track-activities-view]]))
 
 (defmulti views identity)
 (defmethod views :welcome-view [] [welcome-view])
@@ -22,11 +23,14 @@
   [views view-name])
 
 (defn main-view []
-  (re-frame/dispatch [:get-auth0-profile])
   (let [active-view (re-frame/subscribe [:active-view])]
-    (fn []
-      [:div#main
-        [sidebar-component]
-        [:div.content
-          [header-component]
-          [show-view @active-view]]])))
+    (reagent/create-class
+      {:component-will-mount
+       #(re-frame/dispatch [:get-auth0-profile])
+       :reagent-render
+       (fn []
+         [:div#main
+          [sidebar-component]
+          [:div.content
+           [header-component]
+           [show-view @active-view]]])})))
