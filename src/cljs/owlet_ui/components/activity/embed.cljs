399d45db443688ce5970e0a8630a88b9c81b8047
dviramontes <dviramontes@gmail.com>
2016-10-12 01:07:12 -0600

    feat: adds concepts to embed component

--- src/cljs/owlet_ui/components/activity/embed.cljs
+++ src/cljs/owlet_ui/components/activity/embed.cljs
@@ -5,10 +5,16 @@
   (let [activity-data (re/subscribe [:activity-in-view])]
     (fn []
       (let [embed (get-in @activity-data [:fields :embed])
-            preview (get-in @activity-data [:fields :preview :sys :url])]
+            preview (get-in @activity-data [:fields :preview :sys :url])
+            concepts (get-in @activity-data [:fields :concepts])]
         [:div.activity-embed-wrap
           (if (nil? embed)
             [:div.activity-preview
               [:img {:src preview}]]
             [:div.activity-embed {"dangerouslySetInnerHTML"
-                                    #js{:__html embed}}])]))))
+                                    #js{:__html embed}}])
+         [:div.activity-concept-wrap
+          (for [c concepts]
+               ^{:key (gensym "concept-")}
+               [:span.tag.tag-default c])]]))))
+
