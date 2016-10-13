641994d74637a0f83a6fe4710a6d825835126586
eemshi <oreneemshi@gmail.com>
2016-10-12 18:37:20 -0600

    render UNPLUGGED when field is true

--- src/cljs/owlet_ui/components/activity/info.cljs
+++ src/cljs/owlet_ui/components/activity/info.cljs
@@ -12,15 +12,17 @@
 (defn activity-info []
   (let [activity-data (re/subscribe [:activity-in-view])]
     (fn []
-      (let [tech-requirements (get-in @activity-data [:fields :techRequirements])
+      (let [unplugged (get-in @activity-data [:fields :unplugged])
+            tech-requirements (get-in @activity-data [:fields :techRequirements])
             summary (get-in @activity-data [:fields :summary])
             why (get-in @activity-data [:fields :why])
             pre-reqs (get-in @activity-data [:fields :preRequisites])
             materials (get-in @activity-data [:fields :materials])]
         [:div.activity-info-wrap
+         (when unplugged
+           [:button "UNPLUGGED"])
          [set-as-marked "<b>Technology:</b> " tech-requirements]
          [set-as-marked "<b>Summary:</b> " summary]
          [set-as-marked "<b>Why?</b> " why]
          [set-as-marked "<b>Pre-requisites:</b> " pre-reqs]
          [set-as-marked "<b>Materials:</b> " materials]]))))
-
