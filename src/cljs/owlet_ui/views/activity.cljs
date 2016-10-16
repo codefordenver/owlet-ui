4a0e21247e8067323fa222341d598a939691a10c
eemshi <oreneemshi@gmail.com>
2016-10-16 16:11:09 -0600

    init back_track (breadcrumb) component that links back to respective track

--- src/cljs/owlet_ui/views/activity.cljs
+++ src/cljs/owlet_ui/views/activity.cljs
@@ -1,5 +1,6 @@
 (ns owlet-ui.views.activity
-  (:require [owlet-ui.components.activity.title :refer [activity-title]]
+  (:require [owlet-ui.components.activity.back_track :refer [back-track]]
+            [owlet-ui.components.activity.title :refer [activity-title]]
             [owlet-ui.components.activity.embed :refer [activity-embed]]
             [owlet-ui.components.activity.info :refer [activity-info]]
             [owlet-ui.components.activity.inspiration :refer [activity-inspiration]]
@@ -7,7 +8,8 @@
 
 (defn activity-view []
   (fn []
-    [:div.activity-wrap
+    [:div.activity-wrap 
+      [back-track]
       [:div.activity-header.col-xs-12
         [activity-title]]
       [:div.activity-content.col-xs-12.col-lg-6
