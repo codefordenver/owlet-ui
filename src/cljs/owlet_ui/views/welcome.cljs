06c5ca400b033aabb3b228b58a5b3a9f92f75aec
ZadenRB <zaden.ruggieroboune@gmail.com>
2016-10-27 20:54:09 -0600

    Creates and positions teacher/student user select and log in button.

--- src/cljs/owlet_ui/views/welcome.cljs
+++ src/cljs/owlet_ui/views/welcome.cljs
@@ -4,13 +4,30 @@
 
 (defn welcome-view []
       [:div.flexcontainer {:style {:height "100vh"}}
+        [:div.landing]
+        [:div.column-left]
+        [:div.column-right
+          [:form.user-type
+            [:input.user-type {:type "radio"
+                               :name "userType"
+                               :value "student"
+                               :id "student"}]
+            [:label {:for "student"}
+              "Student"]
+            [:input.user-type {:type "radio"
+                               :name "userType"
+                               :value "teacher"
+                               :id "teacher"}]
+            [:label {:for "teacher"}
+              "Teacher"]]]
+        [:div.login
+          [login-component]]
         [:div.flex-item]
         [:div.flex-item
           [:a {:href "#/tracks"}
             [:img {:src "img/landing.png"
                    :width "100%"}]]]
-        [:div.flex-item]
-        [:div.landing]])
+        [:div.flex-item]])
 
         ;;[:div.search.pull-right]
         ;;[:input {:type "search"
