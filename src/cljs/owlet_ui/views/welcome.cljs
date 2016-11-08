e81b5d233415ac5f3239d73522559d08a6f264e8
ZadenRB <zaden.ruggieroboune@gmail.com>
2016-11-07 19:52:11 -0700

    Styles user type selection, sets student as default selection, and adds welcome and description message.

--- src/cljs/owlet_ui/views/welcome.cljs
+++ src/cljs/owlet_ui/views/welcome.cljs
@@ -3,34 +3,37 @@
     [owlet-ui.components.login :refer [login-component]]))
 
 (defn welcome-view []
-      [:div.flexcontainer {:style {:height "100vh"}}
-        [:div.landing]
-        [:div.column-left]
-        [:div.column-right
-          [:form.user-type
-            [:input.user-type {:type "radio"
-                               :name "userType"
-                               :value "student"
-                               :id "student"}]
-            [:label {:for "student"}
-              "Student"]
-            [:input.user-type {:type "radio"
-                               :name "userType"
-                               :value "teacher"
-                               :id "teacher"}]
-            [:label {:for "teacher"}
-              "Teacher"]]]
-        [:div.login
-          [login-component]]
-        [:div.flex-item]
-        [:div.flex-item
-          [:a {:href "#/tracks"}
-            [:img {:src "img/landing.png"
-                   :width "100%"}]]]
-        [:div.flex-item]])
+  [:div.flexcontainer {:style {:height "100vh"}}
+    [:div.landing]
+    [:div.column-left]
+    [:div.column-right
+      [:form.user-type
+        [:div.welcome
+          [:p#largetext "Welcome to Owlet"]
+          [:p#smalltext "To get started, select what type of user you are"]]
+        [:input.user-type {:type "radio"
+                           :name "userType"
+                           :value "student"
+                           :id "student"
+                           :defaultChecked true}]
+        [:label {:for "student"}
+          "Student"]
+        [:input.user-type {:type "radio"
+                           :name "userType"
+                           :value "teacher"
+                           :id "teacher"}]
+        [:label {:for "teacher"}
+          "Teacher"]]]
+    [:div.login
+      [login-component]]
+    [:div.flex-item]
+    [:div.flex-item
+        [:img {:src "img/landing.png"
+               :width "100%"}]]
+    [:div.flex-item]])
 
-        ;;[:div.search.pull-right]
-        ;;[:input {:type "search"
-        ;;         :name "sitesearch"}
-        ;;[:input {:type  "submit"
-        ;;         :value "\uD83D\uDD0D"}]])
+    ;;[:div.search.pull-right]
+    ;;[:input {:type "search"
+    ;;         :name "sitesearch"}
+    ;;[:input {:type  "submit"
+    ;;         :value "\uD83D\uDD0D"}]])
