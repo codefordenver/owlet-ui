fb7c457b431ed512d368975ee2b319b4b71a036e
eemshi <oreneemshi@gmail.com>
2016-11-10 20:44:24 -0700

    remove large sidebar

--- src/cljs/owlet_ui/components/sidebar.cljs
+++ src/cljs/owlet_ui/components/sidebar.cljs
@@ -19,22 +19,4 @@
           [:img {:src "img/icon2.png"}]]
         [:br]
         [:a {:href "#/settings"}
-          [:img {:src "img/icon3.png"}]]]]
-
-    [:div#sidebar
-      [:a {:href "#/"}
-        [:img.owlet {:src "img/owlet-logo.png"}]]
-      [:div.menu
-        [:span
-          [:a {:href "#/"}
-           [:h1 "owlet"]]
-          [:ul.nav.nav-pills.nav-stacked
-            [:li.nav-item
-              [:a.nav-link {:href "#/tracks"}
-                [:img {:src "img/icon1.png"}]]]
-            [:li.nav-item
-              [:a.nav-link {:href "#/"}
-                [:img {:src "img/icon2.png"}]]]
-            [:li.nav-item
-              [:a.nav-link {:href "#/settings"}
-                [:img {:src "img/icon3.png"}]]]]]]]])
+          [:img {:src "img/icon3.png"}]]]]])
