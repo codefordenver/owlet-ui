b470315c01a7dc4165c635b6413d89a12f20116c
eemshi <oreneemshi@gmail.com>
2016-11-16 23:13:17 -0700

    trim zadenMagic2, call it only via :component-did-mount in activity-thumbnail and track-component

--- src/cljs/owlet_ui/components/activity_thumbnail.cljs
+++ src/cljs/owlet_ui/components/activity_thumbnail.cljs
@@ -1,13 +1,19 @@
-(ns owlet-ui.components.activity-thumbnail)
+(ns owlet-ui.components.activity-thumbnail
+    (:require [reagent.core :as reagent]))
 
 (defn activity-thumbnail [fields url]
   (let [image (get-in fields [:preview :sys :url])
         {:keys [title track-id summary]} fields]
-    (fn []
-      [:div.col-lg-4.col-sm-6.col-xs-12
-        [:div.activity-thumbnail-wrap.box-shadow
-          [:a {:href (str "#/tracks/" track-id "/" url)}
-            [:div.activity-thumbnail {:style {:background-image (str "url('" image "')")
-                                              :background-size "cover"}}
-              [:mark.title title]]]
-          [:div.summary summary]]])))
+    (reagent/create-class
+      {:component-did-mount
+       (fn []
+         (js/zadenMagic2))
+       :reagent-render
+        (fn []
+          [:div.col-lg-4.col-sm-6.col-xs-12
+            [:div.activity-thumbnail-wrap.box-shadow
+              [:a {:href (str "#/tracks/" track-id "/" url)}
+                [:div.activity-thumbnail {:style {:background-image (str "url('" image "')")
+                                                  :background-size "cover"}}
+                  [:mark.title title]]]
+              [:div.summary summary]]])})))
