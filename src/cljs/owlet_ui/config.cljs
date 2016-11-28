fcc3dc8c0dc3afa8054d495d3945f5d74e55d676
David A. Viramontes <dviramontes@gmail.com>
2016-11-27 18:15:06 -0700

    Fix 123 (#50)
    * chore: change wording for attribution
    
    * wip
    
    * Adds variable to track previous URL when logging in.
    
    * fix: redirect on login by storing path on localStorage
    
    * fix: redirect on login by setting redirect to false on auth0/lock options
    
    * rm localStorage fix

--- src/cljs/owlet_ui/config.cljs
+++ src/cljs/owlet_ui/config.cljs
@@ -5,7 +5,6 @@
 (def debug?
   ^boolean js/goog.DEBUG)
 
-;; TODO:
 ;; Use a lein env var like the one above
 ;; to toggle this during development
 ;; http://localhost:3000
@@ -15,9 +14,12 @@
 (when debug?
   (enable-console-print!))
 
-(def lock (new js/Auth0Lock
-               "aCHybcxZ3qE6nWta60psS0An1jHUlgMm"
-               "codefordenver.auth0.com"))
+(def lock
+  (new js/Auth0Lock
+       "aCHybcxZ3qE6nWta60psS0An1jHUlgMm"
+       "codefordenver.auth0.com"
+       (clj->js {:auth {:redirect false
+                        :responseType "token"}})))
 
 (.on lock "authenticated"
      (fn [auth-res]
@@ -38,4 +40,3 @@
           :authDomain    "owlet-users.firebaseapp.com"
           :databaseURL   "https://owlet-users.firebaseio.com"
           :storageBucket "owlet-users.appspot.com"})
-
