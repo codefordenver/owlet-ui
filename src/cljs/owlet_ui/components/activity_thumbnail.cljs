97c7d265de26209250608e1ef040702533ac2f6f
eemshi <oreneemshi@gmail.com>
2016-11-28 22:23:43 -0700

    move preview image if-condition to let binding

--- src/cljs/owlet_ui/components/activity_thumbnail.cljs
+++ src/cljs/owlet_ui/components/activity_thumbnail.cljs
@@ -2,13 +2,15 @@
     (:require [reagent.core :as reagent]))
 
 (defn activity-thumbnail [fields url]
-  (let [image (get-in fields [:preview :sys :url])
+  (let [image (if (nil? (get-in fields [:preview :sys :url]))
+                "img/owlet-logo.png"
+                (get-in fields [:preview :sys :url]))
         {:keys [title track-id summary]} fields]
     (fn []
       [:div.col-lg-4.col-sm-6.col-xs-12
-        [:div.activity-thumbnail-wrap.box-shadow
-          [:a {:href (str "#/tracks/" track-id "/" url)}
-            [:div.activity-thumbnail {:style {:background-image (str "url('" image "')")
-                                              :background-size "cover"}}
-              [:mark.title title]]]
-          [:div.summary summary]]])))
+          [:div.activity-thumbnail-wrap.box-shadow
+            [:a {:href (str "#/tracks/" track-id "/" url)}
+              [:div.activity-thumbnail {:style {:background-image (str "url('" image "')")
+                                                :background-size "cover"}}
+                [:mark.title title]]]
+            [:div.summary summary]]])))
