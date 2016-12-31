5b40a5dbbaee4f614746235f521c45eb17007b73
Tyler Perkins <tyler_perkins@me.com>
2016-12-30 17:29:44 -0700

    Dirac DevTools support without sacrificing Figwheel goodness!

--- script/repl.clj
+++
@@ -1,8 +0,0 @@
-; This script runs a Figwheel REPL within the Cursive environment. See
-; https://github.com/bhauman/lein-figwheel/wiki/Running-figwheel-in-a-Cursive-Clojure-REPL
-
-(use 'figwheel-sidecar.repl-api)
-
-(start-figwheel!) ;; <-- fetches configuration
-(cljs-repl)
-
