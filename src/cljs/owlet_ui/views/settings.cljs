0ef0f18b79f8e57e4603b2ca8fea367668abf958
David A. Viramontes <dviramontes@gmail.com>
2017-01-01 20:17:23 -0700

    chore: update .gitignore (#57)
    * chore: rm settings-view logic, unused
    
    * chore: updates gitignore

--- src/cljs/owlet_ui/views/settings.cljs
+++ src/cljs/owlet_ui/views/settings.cljs
@@ -1,41 +1,6 @@
-(ns owlet-ui.views.settings
-  (:require
-    [owlet-ui.utils :refer [get-user-cms-profile]]
-    [owlet-ui.config :as config]
-    [reagent.validation :as validation]
-    [reagent.session :as session]
-    [reagent.core :as reagent :refer [atom]]
-    [ajax.core :refer [PUT]]
-    [re-frame.core :as re-frame]))
-
-(def district-id (atom nil))
+(ns owlet-ui.views.settings)
 
 (defn settings-view []
-  (let [sid (re-frame/subscribe [:social-id-subscription])]
-    (reagent/create-class
-      {:component-did-mount
-       (fn []
-         (get-user-cms-profile
-           @sid
-           #(reset! district-id (:district_id %))))
-       :reagent-render
-       (fn []
-         [:div.outer-height-wrap
-           [:div.inner-height-wrap
-             [:h1#title [:mark "My Settings"]]
-             [:div.search
-              [:label "District ID:"
-               [:input.test {:type        "text"
-                             :value       @district-id
-                             :placeholder 123456
-                             :max-length  6
-                             :on-change   #(reset! district-id (-> % .-target .-value))}]
-               [:input {:type     "submit"
-                        :value    "Enter"
-                        :on-click #(when (and (validation/has-value? (session/get :user-id))
-                                              (validation/has-value? @district-id))
-                                    (PUT (str config/server-url "/api/users-district-id")
-                                         {:params  {:district-id @district-id
-                                                    :user-id     (session/get :user-id)}
-                                          :handler (fn [res]
-                                                     (js/alert res))}))}]]]]])})))
+  [:div.outer-height-wrap
+   [:div.inner-height-wrap
+    [:h1#title [:mark "My Settings"]]]])
