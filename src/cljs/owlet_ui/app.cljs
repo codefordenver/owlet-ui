77481231cf7831159f28d9e4acfc02ef58cafbbb
eemshi <oreneemshi@gmail.com>
2017-01-02 02:02:17 -0500

    rm zadenMagic from branch.cljs, rm commented code

--- src/cljs/owlet_ui/app.cljs
+++ src/cljs/owlet_ui/app.cljs
@@ -4,7 +4,6 @@
             [owlet-ui.components.upload-image-modal :refer [upload-image-component]]
             [owlet-ui.components.sidebar :refer [sidebar-component]]
             [owlet-ui.components.lpsidebar :refer [lpsidebar-component]]
-            ; [owlet-ui.components.header :refer [header-component]]
             [owlet-ui.components.loading :refer [loading-component]]
             [owlet-ui.views.welcome :refer [welcome-view]]
             [owlet-ui.views.activity :refer [activity-view]]
