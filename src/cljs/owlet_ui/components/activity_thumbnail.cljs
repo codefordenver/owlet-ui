8403c960868fc2c19d06da1b89c283cd6e024f4a
dviramontes <dviramontes@gmail.com>
2017-01-14 21:53:27 -0700

    refactor: activity-thumbnail component

--- src/cljs/owlet_ui/components/activity_thumbnail.cljs
+++ src/cljs/owlet_ui/components/activity_thumbnail.cljs
@@ -11,19 +11,18 @@
 (defn activity-thumbnail [fields entry-id]
   (let [preview-image-url (get-in fields [:preview :sys :url])
         image (or preview-image-url "img/default-thumbnail.png")
-        {:keys [title summary techRequirements]} fields]
-    (fn []
-      [:div.col-xs-12.col-md-6.col-lg-4
-       [:div.activity-thumbnail-wrap.box-shadow
-        [:a {:href     (str "#/activity/" entry-id)
-             :on-click #(rf/dispatch [:set-activity-in-view entry-id])}
-         [:div.activity-thumbnail {:style {:background-image (str "url('" image "')")
-                                           :background-size  "cover"}}
-          [:mark.title title]]]
-        (when techRequirements
-          [:div.technology.btn
-           [set-as-marked techRequirements]])
-        (when unplugged
-          [:div.unplugged.btn.btn-warning
-           "UNPLUGGED"])
-        [:div.summary summary]]])))
+        {:keys [title summary unplugged techRequirements]} fields]
+    [:div.col-xs-12.col-md-6.col-lg-4
+     [:div.activity-thumbnail-wrap.box-shadow
+      [:a {:href     (str "#/activity/" entry-id)
+           :on-click #(rf/dispatch [:set-activity-in-view entry-id])}
+       [:div.activity-thumbnail {:style {:background-image (str "url('" image "')")
+                                         :background-size  "cover"}}
+        [:mark.title title]]]
+      (when techRequirements
+        [:div.technology.btn
+         [set-as-marked techRequirements]])
+      (when unplugged
+        [:div.unplugged.btn.btn-warning
+         "UNPLUGGED"])
+      [:div.summary summary]]]))
