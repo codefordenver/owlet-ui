ecd8145af0c6d2c5e2a42bf50f5f7b3f4247418d
dviramontes <dviramontes@gmail.com>
2017-01-17 19:36:18 -0700

    refactor: branch-activities-view component

--- src/cljs/owlet_ui/views/branch_activities.cljs
+++ src/cljs/owlet_ui/views/branch_activities.cljs
@@ -18,10 +18,11 @@
          [:h2 [:mark.white.box.box-shadow [:b "This branch does not exist"]]]
          [:h2 [:mark.white.box-shadow [:b display-name]]
           [:div.flexcontainer-wrap
-           (if (empty? activities)
-             [:p.no-activities [:mark "No activities in this branch yet. Check back soon."]]
+           (if (seq activities)
              (for [activity activities
                    :let [fields (:fields activity)
                          id (get-in fields [:preview :sys :id] (gensym "key-"))
                          entry-id (get-in activity [:sys :id])]]
-               ^{:key id} [activity-thumbnail fields entry-id]))]])]]]))
+               ^{:key id} [activity-thumbnail fields entry-id])
+             [:p.no-activities [:mark "No activities in this branch yet. Check back soon."]])]])]]]))
+
