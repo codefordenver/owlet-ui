e69db351cf3b41899e8210833a3da6f644058bdb
ZadenRB <zaden.ruggieroboune@gmail.com>
2017-01-26 19:39:28 -0700

    Fixes margin errors in branch activites vew and fixes height issues on larger monitors

--- src/cljs/owlet_ui/app.cljs
+++ src/cljs/owlet_ui/app.cljs
@@ -53,22 +53,21 @@
 
             [:div#sidebar-wrap.hidden-sm-down
              [sidebar-component]]
-            [:div.content {:style {:width            "100%"
-                                   :background-image (str "url(" @src ")")
-                                   :background-size  "cover"}}
-             [:div.outer-height-wrap
+            [:div.outer-height-wrap
               [:div.inner-height-wrap
-               [upload-image-component show? close-modal]
-               [:button#change-header-btn
-                {:type     "button"
-                 :class    "btn btn-secondary"
-                 :style    {:font-size "1em"
-                            :padding   "6px"
-                            :display   (if @is-user-logged-in?
-                                         "block"
-                                         "none")}
-                 :on-click open-modal}
-                [:i.fa.fa-pencil-square-o]]
-               (when @loading?
-                 [loading-component])
-               [show-view @active-view]]]]]))})))
+                [:div.content {:style {:background-image (str "url(" @src ")")
+                                       :background-size  "cover"}}
+                   [upload-image-component show? close-modal]
+                   [:button#change-header-btn
+                    {:type     "button"
+                     :class    "btn btn-secondary"
+                     :style    {:font-size "1em"
+                                :padding   "6px"
+                                :display   (if @is-user-logged-in?
+                                             "block"
+                                             "none")}
+                     :on-click open-modal}
+                    [:i.fa.fa-pencil-square-o]]
+                   (when @loading?
+                     [loading-component])
+                   [show-view @active-view]]]]]))})))
