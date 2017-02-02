5f06b9d42e6b78f83ef7913eeae1877fd775dfbe
dviramontes <dviramontes@gmail.com>
2017-02-01 22:27:47 -0700

    feat: adds more 404 copy

--- src/cljs/owlet_ui/views/not-found.cljs
+++ src/cljs/owlet_ui/views/not-found.cljs
@@ -1,8 +1,9 @@
 (ns owlet-ui.views.not-found)
 
-
 (defn not-found-view []
   [:div.not-found
    [:h2 [:a {:href "#/branches"} [:img.back {:src "img/back-filled.png"}]]
         [:mark.white.box-shadow "Error 404 - Not Found"]]
-   [:h3 [:mark.white.box-shadow "Please return to branches view"]]])
+   [:h3 [:mark.white "Hey there, this activity has moved to another URL."]]
+   [:h3 [:mark.white "Chances are you'll find it inside a branch on the"]]
+   [:h3 [:mark.white [:a {:href "#/branches"} "Branches page :)"]]]])
