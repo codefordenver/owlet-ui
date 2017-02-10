cee8701d7ac81c72ea89c57b5d10040c2810d8ec
Connor Ford <git@cjford.net>
2017-02-10 14:57:58 -0700

    Adds Disqus comments for activities (#156)

---
+++ src/cljs/owlet_ui/components/activity/comments.cljs
@@ -0,0 +1,15 @@
+(ns owlet-ui.components.activity.comments
+  (:require [reagent.core :as reagent]
+            [re-frame.core :as re]))
+
+(defn load-comments [activity]
+  (js/disqusReset
+    (get-in activity [:sys :id])
+    (-> js/document .-location .-href)
+    (get-in activity [:fields :title])))
+
+(defn activity-comments []
+  (let [activity @(re/subscribe [:activity-in-view])]
+    (reagent/create-class
+       {:component-did-mount #(load-comments activity)
+        :reagent-render ( fn [] [:div.activity-comments-wrap [:div#disqus_thread]] )})))
