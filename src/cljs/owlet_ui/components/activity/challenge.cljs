1b9fffe0077a79af072312cbad2651867f5c0a47
eemshi <oreneemshi@gmail.com>
2017-02-09 19:29:21 -0700

    push for travis build

--- src/cljs/owlet_ui/components/activity/challenge.cljs
+++ src/cljs/owlet_ui/components/activity/challenge.cljs
@@ -6,6 +6,6 @@
 (defn activity-challenge [challenge]
   [:div.activity-challenge-wrap.box-shadow
     [:div.list-title
-     [:p [:b "⚡⚡ challenge⚡⚡"]]]
+     [:p [:b "⚡⚡ challenge⚡⚡ "]]]
     [:div {"dangerouslySetInnerHTML"
            #js{:__html (.makeHtml showdown challenge)}}]])
