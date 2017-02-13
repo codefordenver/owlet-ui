deb58bfa6b08bbc617bcbc87b91a7e58fcba8f9b
dviramontes <dviramontes@gmail.com>
2017-02-12 17:37:01 -0700

    fix: adds clojur.spec check-and-throw fn to helpers/keywordize-name

--- src/cljs/owlet_ui/helpers.cljs
+++ src/cljs/owlet_ui/helpers.cljs
@@ -1,8 +1,18 @@
 (ns owlet-ui.helpers
   (:require [clojure.string :as clj->str]
+            [cljs.spec :as s]
             [camel-snake-kebab.core :refer [->kebab-case]]))
 
+(s/def ::string? string?)
+
+(defn check-and-throw
+  "throw an exception if value doesn't match the spec"
+  [a-spec val]
+  (when-not (s/valid? a-spec val)
+    (throw (ex-info (str "spec failed because: " (s/explain-str a-spec val)) {}))))
+
 (defn keywordize-name [name]
+  (check-and-throw ::string? name)
   (-> name ->kebab-case keyword))
 
 (def remove-nil (partial remove nil?))
--- src/cljs/owlet_ui/views/search_results.cljs
+++ src/cljs/owlet_ui/views/search_results.cljs
@@ -3,5 +3,10 @@
             [owlet-ui.components.activity-thumbnail :refer [activity-thumbnail]]
             [owlet-ui.components.back :refer [back]]))
 
+;; NOTE: (zaden) might not need this view but lets keep it around
+;; in case we find that branch-activities doesnt really fit our needs..
+
 (defn search-results-view []
-  [])
+  ;; NOTE: (zaden) returning empty vector throws error,
+  ;; so lets return this empty :div for now
+  [:div])
