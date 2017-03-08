0ad74b5d54a9ddf92ca97f9c1a82f1ee5168788e
ZadenRB <zaden.ruggieroboune@gmail.com>
2017-03-07 19:50:18 -0700

    Fixes disques appendChild null error by moving a portion of disqus config into comments.cljs

--- resources/public/js/vendor/disqus.js
+++ resources/public/js/vendor/disqus.js
@@ -1,23 +1,5 @@
-var disqus_shortname = 'owlet-2';
-
-/* * * DON'T EDIT BELOW THIS LINE * * */
-(function () {
-    var dsq = document.createElement('script');
-    dsq.type = 'text/javascript';
-    dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
-
-window.disqusReset = function (identifier, url, title) {
-    if (DISQUS) {
-        DISQUS.reset({
-            reload: true,
-            config: function () {
-                this.page.identifier = identifier;
-                this.page.url = url;
-                this.page.title = title;
-            }
-        });
-    }
-};
+var disqus_config  = function () {
+                this.page.identifier = window.location.href.substr(window.location.href.lastIndexOf('/') + 1).substr(2);
+                this.page.url = window.location.href;
+                this.page.title = window.title;
+              }
--- src/cljs/owlet_ui/components/activity/comments.cljs
+++ src/cljs/owlet_ui/components/activity/comments.cljs
@@ -1,15 +1,22 @@
 (ns owlet-ui.components.activity.comments
   (:require [reagent.core :as reagent]
-            [re-frame.core :as re]))
+            [re-frame.core :as re]
+            [clojure.core :refer-macros [this-as]]))
 
 (defn load-comments [activity]
-  (js/disqusReset
-    (get-in activity [:sys :id])
-    (-> js/document .-location .-href)
-    (get-in activity [:fields :title])))
+  (let [dsq (js/document.createElement "script")
+        body (aget (js/document.getElementsByTagName "body") 0)]
+    (set! (-> dsq .-type) "text/javascript")
+    (set! (-> dsq .-async) true)
+    (set! (-> dsq .-src) "//owlet-2.disqus.com/embed.js")
+    (.appendChild body dsq)))
+
+
+
 
 (defn activity-comments []
   (let [activity @(re/subscribe [:activity-in-view])]
     (reagent/create-class
-       {:component-did-mount #(load-comments activity)
-        :reagent-render ( fn [] [:div.activity-comments-wrap [:div#disqus_thread]] )})))
+       {:component-did-mount (fn []
+                                (load-comments activity))
+        :reagent-render ( fn [] [:div.activity-comments-wrap [:div#disqus_thread]])})))
