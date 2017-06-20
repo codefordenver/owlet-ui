88e2bb6ca4cf3e01d4317635716fa1ebdd53a57d
eemshi <oreneemshi@gmail.com>
2017-06-19 20:30:16 -0600

    adds PUT request for email-endpoint

--- src/cljs/owlet_ui/components/email_notification.cljs
+++ src/cljs/owlet_ui/components/email_notification.cljs
@@ -1,10 +1,36 @@
 (ns owlet-ui.components.email-notification
   (:require [re-frame.core :as rf]
-            [reagent.core :as reagent]))
+            [reagent.core :as reagent]
+            [ajax.core :as ajax :refer [GET POST PUT]]
+            [owlet-ui.config :as config]))
+
+(defonce email-endpoint
+         (str config/server-url
+              "/owlet/webhook/content/subscribe"))
+
+(def res (reagent/atom nil))
+
+(defn subscribe [email]
+  (PUT email-endpoint {:params {:email email}
+                       :format :json
+                       :handler #(reset! res true)
+                       :error-handler #(reset! res false)}))
 
 (defn email-notification []
-  [:div#email-wrap
-    [:p#email-text "Notify me when new activities are added"]
-    [:input#email-input {:type "text"
-                         :placeholder "Email address"}]
-    [:button#email-button "Subscribe"]])
+  (let [email (reagent/atom nil)]
+    (fn []
+      [:div#email-wrap {:style {:background-color (cond
+                                                    (= @res true) "green"
+                                                    (= @res false) "red")}}
+       (cond
+         (= @res true) "Success"
+         (= @res false) "Failed"
+         :else
+           [:div
+            [:p#email-text "Notify me when new activities are added"]
+            [:input#email-input {:type "text"
+                                 :placeholder "Email address"
+                                 :on-change #(reset! email (-> % .-target .-value))
+                                 :value @email}]
+            [:button#email-button {:on-click #(subscribe @email)}
+              "Subscribe"]])])))
