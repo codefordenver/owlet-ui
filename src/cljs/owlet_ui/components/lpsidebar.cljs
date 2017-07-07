fe9bab1224a29ff463be5d2e359f0a6dd27024c6
ZadenRB <zaden.ruggieroboune@gmail.com>
2017-07-06 21:44:51 -0600

    Renames if statement, uses it for sidebar width as well as toggle position

--- src/cljs/owlet_ui/components/lpsidebar.cljs
+++ src/cljs/owlet_ui/components/lpsidebar.cljs
@@ -10,31 +10,30 @@
   (swap! lpsidebar-state not))
 
 (defn lpsidebar-component []
-    [:div
-     (if @lpsidebar-state
-      [:div.lpsidebar-overlay.hidden-md-up.opened-sidebar {:on-click #(toggle-lpsidebar)}]
-      [:div.lpsidebar-overlay.hidden-md-up])
-     [:div.lpsidebar-wrap.hidden-md-up
-      [:div.lpsidebar {:style {:width (if @lpsidebar-state
-                                        "80px"
-                                        "0")}}
-       [:div#owlet-logo-div
-        [:a#owlet-image {:href "#/"
-                         :on-click #(toggle-lpsidebar)}
-         [:img#owlet-owl {:src "../img/owlet-owl.png"}]]]
-       [:div.menu
-        [:div.login {:on-click #(toggle-lpsidebar)}
-         [login-component]]
-        [:a.navigation {:href     "#/branches"
-                        :on-click #(toggle-lpsidebar)}
-         [:img {:src "img/icon1.png"}]]]
-       (let [left? (if @lpsidebar-state
+  (fn []
+    (let [position? (if @lpsidebar-state
                       "80px"
                       "0")]
+      [:div
+       (if @lpsidebar-state
+        [:div.lpsidebar-overlay.hidden-md-up.opened-sidebar {:on-click #(toggle-lpsidebar)}]
+        [:div.lpsidebar-overlay.hidden-md-up])
+       [:div.lpsidebar-wrap.hidden-md-up
+        [:div.lpsidebar {:style {:width position?}}
+         [:div#owlet-logo-div
+          [:a#owlet-image {:href "#/"
+                           :on-click #(toggle-lpsidebar)}
+           [:img#owlet-owl {:src "../img/owlet-owl.png"}]]]
+         [:div.menu
+          [:div.login {:on-click #(toggle-lpsidebar)}
+           [login-component]]
+          [:a.navigation {:href     "#/branches"
+                          :on-click #(toggle-lpsidebar)}
+           [:img {:src "img/icon1.png"}]]]
          (if @lpsidebar-state
-           [:img#lpsidebar-opened.lpsidebar-toggle.hidden-md-up {:style {:left left?}
+           [:img#lpsidebar-opened.lpsidebar-toggle.hidden-md-up {:style {:left position?}
                                                                  :src      "img/owlet-tab-opened.png"
                                                                  :on-click #(toggle-lpsidebar)}]
-           [:img#lpsidebar-closed.lpsidebar-toggle.hidden-md-up {:style {:left left?}
+           [:img#lpsidebar-closed.lpsidebar-toggle.hidden-md-up {:style {:left position?}
                                                                  :src      "img/owlet-tab-closed.png"
-                                                                 :on-click #(toggle-lpsidebar)}]))]]])
+                                                                 :on-click #(toggle-lpsidebar)}])]]])))
