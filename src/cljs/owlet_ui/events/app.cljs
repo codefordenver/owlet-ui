b69fb0166c222e18e0292b4a38c3b0c46919d361
tylerperkins <tyler_perkins@me.com>
2017-07-17 20:36:11 -0600

    Delete old image files (#168)
    * Added event-type to owlet-ui.firebase/on-change to accommodate changes other than "value" changes.
    
    Tests for being logged now look only at :firebase-id, so components who care just about being logged in won't be unnecessarily redrawn when something else changes in :my-identity.
    
    Firebase data in /users/<firebase-id>/ will only be readable by the currently signed-in user, so its contents are replicated in app-db at path [:my-identity :private].
    
    Added "dev" Firebase project, for development.
    
    * FIX: Data in re-frame.db/app-db being blown away by improper use of re-frame.core/reg-cofx.
    
    FIX: Event [:set-active-view :branches-view] needlessly being dispatched twice from each of welcome.cljs and back.cljs.
    
    FIX: Broken link in not_found.cljs.
    
    * FIX: User background not showing up after login until refreshing the page.
    
    Refactored handlers to better separate concerns.
    
    * Small refactor: Use assoc instead of assoc-in.
    
    * Improved docs.
    
    * Upgraded versions of Figwheel Sidecar, re-frame, re-frisk, and firebase. Also removed dependency reagent, since it is included by re-frame.
    
    * Reorganized Firebase data to denormalize `presence`, decoupling it from `classes`. Then we don't need to know the user's class at login time.
    
    FIX: Bug in owlet-ui.upload-image-modal/upload-form obtaining user's Firebase ID.
    
    * FIX: Latest binaryage/dirac broke because latest version has new requirements. Upgrades led to breaking our build in other ways, such as "REPL server launch timed out".
    
    Took the opportunity to ensure that we're using the latest versions of most of our dependencies, where possible.
    
    Removed unneeded lines in project.clj.
    
    * Finish merge of owlet_ui/events/ changes from master.
    
    * FIX: Image files were accumulating in Firebase Storage. When the user uploaded a new image, the old one was not deleted.
    
    Refactored owlet-ui.firebase and added ability to fire events on completion of deleting a file or when any Promise completes.
    
    * Fixed issue where same uploaded file was deleted and simplified distinguishing image files using filename instead of URL.

--- src/cljs/owlet_ui/events/app.cljs
+++ src/cljs/owlet_ui/events/app.cljs
@@ -62,9 +62,54 @@
 
 (rf/reg-event-fx
   :update-user-background!
-  (fn [{{{my-db-ref :private-ref} :my-identity} :db} [_ url]]
-    {:firebase-reset-into-ref [my-db-ref {:background-image-url url}]
-     :dispatch                [:show-bg-img-upload false]}))
+  (fn [{{{:keys [private private-ref]} :my-identity} :db}
+       [_ {:keys [url filename]}]]
+    ; Called after the upload of a background image file was successful, we
+    ; update the user's data in the Firebase database, which will automatically
+    ; cause app-db to be similarly updated, thanks to the effect
+    ; :start-authorized-listening and the handler :private.
+    (let [old-url        (:background-image-url  private)
+          old-name       (:background-image-name private)
+          old-image-info {:background-image-url  old-url
+                          :background-image-name old-name}
+          new-image-info {:background-image-url  url
+                          :background-image-name filename}]
+
+      {:firebase-reset-into-ref
+       [private-ref new-image-info            ; Where and what to save.
+        :user-background-saved new-image-info old-image-info]
+                                              ; Event to dispatch when complete.
+       :dispatch
+       [:show-bg-img-upload false]})))
+
+
+(rf/reg-event-fx
+  :user-background-saved
+  (fn [_ [_ {err :error-reason} new-image-info old-image-info]]
+    ; Called after an attempt to update the Firebase db with the URL and
+    ; filename of a new background image for the user, we delete the old image
+    ; file if the update was successful, or we delete the new image file if
+    ; there was an error.
+
+    (letfn [(same-image-name-in [& infos]
+              ; Return true iff all the infos maps refer to the same filename.
+              (apply = (map :background-image-name infos)))]
+
+      ; Delete the old uploaded file only if Firebase still has it. The old
+      ; file will already have been replaced if it had the same filename as the
+      ; new file we just uploaded. In this case, proceeding would just result
+      ; in deleting the new file we just uploaded! Also, if we should indeed
+      ; delete, and if there was an error attempting to write the new URL to
+      ; the Firebase DATABASE (at users/.../background-image-url), then we do
+      ; delete the file we just uploaded, because it would be inaccessible by
+      ; the app.
+      (if-let [url-to-delete (:background-image-url (if err
+                                                       new-image-info
+                                                       old-image-info))]
+        (when (not (same-image-name-in new-image-info old-image-info))
+          {:delete-file-at-url url-to-delete})))))
+
+      ; Do no effects if the filename didn't change: return nil.
 
 
 (rf/reg-event-db
