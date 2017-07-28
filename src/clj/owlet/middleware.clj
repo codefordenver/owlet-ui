580c626bef12f02d5b4277ebab23bb55fb16b0d4
ZadenRB <zaden.ruggieroboune@gmail.com>
2017-07-27 21:24:07 -0600

    Fixes middleware, moves email templates into correct spot (#173)

--- src/clj/owlet/middleware.clj
+++ src/clj/owlet/middleware.clj
@@ -1,41 +1,13 @@
 (ns owlet.middleware
   (:require [owlet.env :refer [defaults]]
-            [clojure.tools.logging :as log]
-            [owlet.layout :refer [*app-context* error-page]]
-            [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
-            [ring.middleware.webjars :refer [wrap-webjars]]
-            [ring.middleware.cors :refer [wrap-cors]]
-            [ring.middleware.json :refer [wrap-json-response]]
-            [muuntaja.middleware :refer [wrap-format wrap-params]]
             [owlet.config :refer [env]]
+            [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
+            [owlet.layout :refer [*app-context* error-page]]
+            [ring.middleware.cors :refer [wrap-cors]]
             [ring-ttl-session.core :refer [ttl-memory-store]]
-            [ring.middleware.defaults :refer [site-defaults wrap-defaults]])
-  (:import [javax.servlet ServletContext]))
-
-(defn wrap-context [handler]
-  (fn [request]
-    (binding [*app-context*
-              (if-let [context (:servlet-context request)]
-                ;; If we're not inside a servlet environment
-                ;; (for example when using mock requests), then
-                ;; .getContextPath might not exist
-                (try (.getContextPath ^ServletContext context)
-                     (catch IllegalArgumentException _ context))
-                ;; if the context is not specified in the request
-                ;; we check if one has been specified in the environment
-                ;; instead
-                (:app-context env))]
-      (handler request))))
-
-(defn wrap-internal-error [handler]
-  (fn [req]
-    (try
-      (handler req)
-      (catch Throwable t
-        (log/error t)
-        (error-page {:status 500
-                     :title "Something very bad has happened!"
-                     :message "We've dispatched a team of highly trained gnomes to take care of the problem."})))))
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.format :refer [wrap-restful-format]]
+            [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
 
 (defn wrap-csrf [handler]
   (wrap-anti-forgery
@@ -46,7 +18,9 @@
         :title "Invalid anti-forgery token"})}))
 
 (defn wrap-formats [handler]
-  (let [wrapped (-> handler wrap-params wrap-format)]
+  (let [wrapped (wrap-restful-format
+                  handler
+                  {:formats [:json-kw :transit-json :transit-msgpack]})]
     (fn [request]
       ;; disable wrap-formats for websockets
       ;; since they're not compatible with this middleware
@@ -55,12 +29,10 @@
 (defn wrap-base [handler]
   (-> ((:middleware defaults) handler)
       wrap-json-response
-      wrap-webjars
+      wrap-formats
       (wrap-defaults
         (-> site-defaults
             (assoc-in [:security :anti-forgery] false)
             (assoc-in  [:session :store] (ttl-memory-store (* 60 30)))))
       (wrap-cors :access-control-allow-origin [#".+"]
-                 :access-control-allow-methods [:get :post :put :delete])
-      wrap-context
-      wrap-internal-error))
+                 :access-control-allow-methods [:get :post :put :delete])))
