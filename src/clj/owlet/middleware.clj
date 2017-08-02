bf8c237fbabc906b339201207ea4af7a9b98e4d4
David Viramontes <dviramontes@gmail.com>
2017-08-01 20:41:48 -0600

    fix: puts back required middleware for serving templates

--- src/clj/owlet/middleware.clj
+++ src/clj/owlet/middleware.clj
@@ -7,7 +7,34 @@
             [ring-ttl-session.core :refer [ttl-memory-store]]
             [ring.middleware.json :refer [wrap-json-response]]
             [ring.middleware.format :refer [wrap-restful-format]]
-            [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
+            [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+            [clojure.tools.logging :as log])
+  (:import [javax.servlet ServletContext]))
+
+(defn wrap-context [handler]
+  (fn [request]
+    (binding [*app-context*
+              (if-let [context (:servlet-context request)]
+                ;; If we're not inside a servlet environment
+                ;; (for example when using mock requests), then
+                ;; .getContextPath might not exist
+                (try (.getContextPath ^ServletContext context)
+                     (catch IllegalArgumentException _ context))
+                ;; if the context is not specified in the request
+                ;; we check if one has been specified in the environment
+                ;; instead
+                (:app-context env))]
+      (handler request))))
+
+(defn wrap-internal-error [handler]
+  (fn [req]
+    (try
+      (handler req)
+      (catch Throwable t
+        (log/error t)
+        (error-page {:status 500
+                     :title "Something very bad has happened!"
+                     :message "We've dispatched a team of highly trained gnomes to take care of the problem."})))))
 
 (defn wrap-csrf [handler]
   (wrap-anti-forgery
@@ -35,4 +62,6 @@
             (assoc-in [:security :anti-forgery] false)
             (assoc-in  [:session :store] (ttl-memory-store (* 60 30)))))
       (wrap-cors :access-control-allow-origin [#".+"]
-                 :access-control-allow-methods [:get :post :put :delete])))
+                 :access-control-allow-methods [:get :post :put :delete])
+      wrap-context
+      wrap-internal-error))
